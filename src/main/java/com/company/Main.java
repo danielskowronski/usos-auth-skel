9be2d6b505cf5df284b3c2bdb75ef2f1475e81d9
Daniel Skowroński <daniel@dsinf.net>
2016-04-28 19:22:23 +0200

    auth flow works

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
@@ -9,15 +10,18 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.nio.entity.ContentInputStream;
 import org.apache.http.util.EntityUtils;
 import sun.misc.IOUtils;
 
+import javax.xml.ws.spi.http.HttpContext;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Set;
 import java.util.concurrent.Future;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,7 +34,7 @@ public class Main {
     private static String URL_LOGIN = "https://login.uj.edu.pl/login?service=https%3A%2F%2Fwww.usosweb.uj.edu.pl%2Fkontroler.php%3F_action%3Dlogowaniecas%2Findex&locale=pl";
 
     public static void main(String[] args) throws Exception {
-        CloseableHttpAsyncClient httpclient = HttpAsyncClients.custom().build();
+        CloseableHttpAsyncClient httpclient = HttpAsyncClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
         httpclient.start();
         try {
             HttpGet httpget = new HttpGet(URL_LOGIN);
@@ -45,14 +49,13 @@ public class Main {
             m.find(); String  jakis_token = m.group(1);
             System.out.println("jakis_token = "+jakis_token);
 
-
-            //chyba między sesjami trzeba trzymać ciasteczko JSESSIONID=125B9DBD28C85F1042952AA295E68EAD
             HttpPost httpPost = new HttpPost(URL_LOGIN);
-            String postparams = "username=daniel.skowronski@student.uj.edu.pl&password=XD&lt="+jakis_token+"&_eventId=submit&submit=zaloguj";
+            String postparams = "username=daniel.skowronski%40student.uj.edu.pl&password=xD&lt="+jakis_token+"&_eventId=submit&submit=zaloguj";
             entity = new ByteArrayEntity(postparams.getBytes("UTF-8"));
             httpPost.setEntity(entity);
+            httpPost.setHeader("Content-Type","application/x-www-form-urlencoded");
             System.out.println("Executing request " + httpPost.getRequestLine());
-            future = httpclient.execute(httpPost, null);
+            future = httpclient.execute(httpPost,null);
             response = future.get();
             System.out.println("Response: " + response.getStatusLine());
             entity = response.getEntity();
